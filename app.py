0299ac34024803069cbb737d26d03311ccba0e15
M Shaafi Jahangir <mshaafijahangir@gmail.com>
2025-03-21 00:53:21 -0700

    add, delete, update works

--- app.py
+++ app.py
@@ -17,6 +17,12 @@ class User(db.Model):
     def __repr__(self):
         return f'<User {self.name}>'
 
+@app.route('/users')
+def users():
+    # Query all users from the database
+    users = User.query.all()
+    return render_template('users.html', users=users)  # Render the users.html template
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
@@ -34,6 +40,47 @@ def form():
         
         return f'User {name} added to the database!'
     return render_template('form.html')
+
+@app.route('/update/<int:user_id>', methods=['GET', 'POST'])
+def update_user(user_id):
+    user = User.query.get_or_404(user_id)
+
+    if request.method == 'POST':
+        # Get the updated data from the form
+        user.name = request.form['username']
+        user.email = request.form['email']
+
+        # Commit the changes to the database
+        db.session.commit()
+        return f'User {user.name} has been updated!'
+
+    return render_template('update_user.html', user=user)
+
+@app.route('/delete/<int:user_id>', methods=['POST'])
+def delete_user(user_id):
+    # Get the user by id
+    user = User.query.get_or_404(user_id)
+
+    # Delete the user from the database
+    db.session.delete(user)
+    db.session.commit()
+
+    return f'User {user.name} has been deleted!'
+
+
+@app.route('/delete_confirm/<int:user_id>', methods=['GET', 'POST'])
+def delete_user_confirm(user_id):
+    user = User.query.get_or_404(user_id)
+
+    if request.method == 'POST':
+        # Delete the user if confirmed
+        db.session.delete(user)
+        db.session.commit()
+        return f'User {user.name} has been deleted!'
+
+    return render_template('delete_confirm.html', user=user)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
